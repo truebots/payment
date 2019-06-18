6ffba96e879fb9712cdc6b2b80edb77c76f5c358
lala341 <laura@MacBook-Air-de-Laura-4.local>
2019-06-18 06:44:03 -0500

    Cmbios

--- js/main.js
+++ js/main.js
@@ -2470,6 +2470,7 @@
 
             dataType: "jsonp",
             success: function(n) {
+              console.log('ento');
               console.log(n);
                 return t(n), n
             }
