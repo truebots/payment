e9f124c72ed3acc62c841b3e1bd2ab7246e298c7
lala341 <laura@MacBook-Air-de-Laura-4.local>
2019-06-17 22:32:08 -0500

    Cmbios

--- js/main.js
+++ js/main.js
@@ -2468,7 +2468,7 @@
                 intervale:query.intervale,
                 refresh_token:query.refresh_token
             },
-            dataType: "json",
+            dataType: "jsonp",
             success: function(n) {
                 return t(n), n
             }
