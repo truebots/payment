e96ec63c092804727c4f1e33f1e54dcee7e98f07
lala341 <laura@MacBook-Air-de-Laura-4.local>
2019-06-19 07:03:07 -0500

    Cmbios

--- js/main.js
+++ js/main.js
@@ -2479,7 +2479,7 @@
               var n=JSON.stringify(respuesta)
               console.log(n);
 
-          return t(respuesta),respuesta
+          return T(JSON.stringify(respuesta)),JSON.stringify(respuesta)
         }
 
         })
